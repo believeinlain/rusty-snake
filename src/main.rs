9bb16aed18fa89ac145f36eeae31fe57008a4159
Stephanie Aelmore <steph.aelmore@gmail.com>
2022-07-31 11:25:47 -1000

    refactored to use if let

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -10,23 +9,27 @@ fn main() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
+    event_loop.run(move |event, _, control_flow| {
+        if let Event::WindowEvent {
             ref event,
             window_id,
-        } if window_id == window.id() => match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
-                        ..
-                    },
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
-        },
-        _ => {}
+        } = event
+        {
+            if window_id == window.id() {
+                if let WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } = event
+                {
+                    *control_flow = ControlFlow::Exit
+                }
+            }
+        }
     });
 }
